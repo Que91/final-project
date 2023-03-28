9abeca746d17a1b06e077f524764ddd8c2d24e1a
Que91 <116540279+Que91@users.noreply.github.com>
2023-03-27 23:13:33 -0400

    CRUD server request done

--- src/context/PostContext.js
+++ src/context/PostContext.js
@@ -1,26 +1,44 @@
-import { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { createContext, useState, useEffect } from "react";
 
 const PostContext = createContext();
 
 export const PostProvider = ({ children }) => {
-  const [post, setPost] = useState([
-    {
-      text: "This item is from context",
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState([]);
 
   const [postEdit, setPostEdit] = useState({
     item: {},
     edit: false,
   });
 
-  const addPost = (createdPost) => {
-    createdPost.id = uuidv4();
-    setPost([createdPost, ...post]);
-    console.log(createdPost);
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const fetchPost = async () => {
+    const response = await fetch(
+      "http://localhost:5000/post?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+    setPost(data);
+    setIsLoading(false);
+  };
+// Add Post
+  const addPost = async (createdPost) => {
+    const response = await fetch("http://localhost:5000/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(createdPost),
+    });
+    const data = await response.json();
+
+    setPost([data, ...post]);
   };
 
+
+  //Delete Post
   const editPost = (item) => {
     setPostEdit({
       item,
@@ -28,14 +46,29 @@ export const PostProvider = ({ children }) => {
     });
   };
 
-  const updatePost = (id, updItem) => {
-    setPost(
-        post.map((item) => (item.id === id ? {...item, ... updItem } : item))
-    )
-  }
 
-  const deletePost = (id) => {
+  //Update Post Item
+  const updatePost = async (id, updItem) => {
+    const response = await fetch (`http://localhost:5000/post/${id}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updItem)
+    } )
+    const data = await response.json()
+    setPost(
+      post.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
+  };
+
+
+  //Delete Post
+  const deletePost = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+        await fetch(`http://localhost:5000/post/${id}`, {
+            method: 'DELETE'
+        })
       setPost(post.filter((item) => item.id !== id));
     }
   };
@@ -45,11 +78,11 @@ export const PostProvider = ({ children }) => {
       value={{
         post,
         postEdit,
+        isLoading,
         addPost,
         editPost,
         updatePost,
         deletePost,
-        
       }}
     >
       {children}
